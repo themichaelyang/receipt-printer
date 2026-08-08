217895e789e6d93e82ae906bcd2cb98af6d2be5f
Michael Yang <themichaelyang@gmail.com>
2026-08-07 17:38:11 -0700

    add newlines to example

--- print.rb
+++ print.rb
@@ -6,12 +6,13 @@ require 'escpos'
 epson_printer = EpsonTM88III.new
 commands = Escpos::Printer.new
 commands << 'hello world'
+commands << "\n\n\n" # these newlines are needed, for some reason. or it won't print.
 commands.cut!
 p epson_printer.write(commands.to_escpos)
 
-STATUS_CHECK = [
-  0x1B, 0x40,        # ESC @  (initialize, optional)
-  0x10, 0x04, 0x01   # DLE EOT 1
-]
-epson_printer.write(STATUS_CHECK.pack('C*'))
-p epson_printer.read(8)
+# STATUS_CHECK = [
+#   0x1B, 0x40,        # ESC @  (initialize, optional)
+#   0x10, 0x04, 0x01   # DLE EOT 1
+# ]
+# epson_printer.write(STATUS_CHECK.pack('C*'))
+# p epson_printer.read(8)
